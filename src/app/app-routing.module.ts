c34e1e1a19a79e55d75a251c74ee1c82d386ac84
Girish <94887830+ayushi-img@users.noreply.github.com>
2022-01-27 17:56:55 +0530

    setup routing  and  some changes in login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { UserexperienceComponent } from './userexperience/userexperience.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpComponent } from './help/help.component';
+import { AboutComponent } from './about/about.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
@@ -9,7 +13,11 @@ const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
-  { path: "signup", component: SignupComponent }
+  { path: "signup", component: SignupComponent },
+  { path: "aboutus", component: AboutComponent },
+  { path: "help", component: HelpComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "user-experience", component: UserexperienceComponent },
 ];
 
 @NgModule({
